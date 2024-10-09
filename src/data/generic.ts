1d21c3032568e3b80573eecb9b86f91954a6c59b
Jake Balas <jake@balas.io>
2024-10-09 14:51:37 +0200

    Add basic data

---
+++ src/data/generic.ts
@@ -0,0 +1,8 @@
+export const genericInformation = {
+    pageTitle: "Python Pizza Brno",
+    socialDescription: "Python Pizza Brno",
+    siteHeading: "Brno Python Pizza",
+    subHeading: "Clubco Brno Vlněna, Vlněna 5",
+    whenDatetime: "2025/02/22 10:00 AM UTC",
+    whenFormat: "do MMMM Y",
+};
