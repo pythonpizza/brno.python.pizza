00c649919f84215142c3615220b77d3331489d6e
Jake Balas <jake@balas.io>
2024-10-15 19:39:01 +0200

    remove unused static prefix and disable cache for now

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: process.env.ASSET_PREFIX,
   basePath: "/brno.python.pizza",
 };
 module.exports = nextConfig;
