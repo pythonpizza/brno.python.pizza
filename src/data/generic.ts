f9573b393f9c4d9ade8296b8ee3e6e75ef334a3d
Jake Balas <jake@balas.io>
2024-10-16 20:01:51 +0200

    Shorten venue address

--- src/data/generic.ts
+++ src/data/generic.ts
@@ -2,7 +2,7 @@ export const genericInformation = {
   pageTitle: "Python Pizza Brno",
   socialDescription: "Python Pizza Brno",
   siteHeading: "Brno Python Pizza",
-  subHeading: "Clubco Brno Vlněna, Vlněna 5",
+  subHeading: "Clubco Brno, Vlněna 5",
   whenDatetime: "2025/02/22 10:00 AM UTC",
   whenFormat: "do MMMM y",
 };
