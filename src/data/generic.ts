45f4cdfb0f86f38adb13885090612799d7f87ee8
Jake Balas <jake@balas.io>
2024-10-17 23:52:01 +0200

    expand socials text

--- src/data/generic.ts
+++ src/data/generic.ts
@@ -1,6 +1,6 @@
 export const genericInformation = {
   pageTitle: "Python Pizza Brno",
-  socialDescription: "Python Pizza Brno",
+  socialDescription: "Python Pizza Brno, join us @ Clubco Brno, Vlněna 5 on 22nd February 2025",
   siteHeading: "Brno Python Pizza",
   subHeading: "Clubco Brno, Vlněna 5",
   whenDatetime: "2025/02/22 10:00 AM UTC",
