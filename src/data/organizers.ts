ef3804020d9965a0b53c559d9aa5b9e506619e78
Jake Balas <jake@balas.io>
2024-10-18 10:54:45 +0200

    Refactor orgs section a bit, add Jake pic

---
+++ src/data/organizers.ts
@@ -0,0 +1,23 @@
+export type Organizer = {
+  name: string;
+  image: string;
+  index: number;
+};
+
+export const ORGANIZERS: Organizer[] = [
+  {
+    name: "Anežka Müller",
+    image: "speakers/pizza-square.png",
+    index: 1
+  },
+  {
+    name: "Moisés Guimarães de Medeiros",
+    image: "organizers/moises.jpg",
+    index: 2,
+  },
+  {
+    name: "Jake Baláš",
+    image: "organizers/jake.png",
+    index: 3
+  },
+];
--- src/sections/organizers.tsx
+++ src/sections/organizers.tsx
@@ -2,16 +2,8 @@ import React from "react";
 import { Container, Grid, Heading, Paragraph } from "theme-ui";
 
 import Organizer from "./organizer";
+import { ORGANIZERS } from "~/data/organizers";
 
-const orgData = [
-  { name: "Anežka Müller", image: "speakers/pizza-square.png", index: 1 },
-  {
-    name: "Moisés Guimarães de Medeiros",
-    image: "speakers/moises.jpg",
-    index: 2,
-  },
-  { name: "Jake Baláš", image: "speakers/pizza-square.png", index: 3 },
-];
 
 export const OrganizerSection = () => {
   return (
@@ -34,7 +26,7 @@ export const OrganizerSection = () => {
         }}
       >
         <Grid columns={[2, 2, 2, 3]} gap="primary">
-          {orgData.map((org) => (
+          {ORGANIZERS.map((org) => (
             <Organizer key={org.index} name={org.name} image={org.image} />
           ))}
         </Grid>
